6cb90cbfb0b70342248fd791de606f1233ac2464
David Pickart <davidstoreypickart@gmail.com>
2017-08-16 15:57:53 -0500

    Add docs for configureApi and configureHttp

--- src/configure-api.js
+++ src/configure-api.js
@@ -1,5 +1,26 @@
 import { api } from './api'
 
+/**
+ * 
+ * A function that returns a configured instance of the {@link api} module.
+ * This function's argument will be used as the default config for the returned instance.
+ *
+ * Note: This configuration can always be overridden by passing in options manually.
+ *
+ * @name configureApi
+ * @type Function
+ * @param {Object} config - An api configuration object
+ * @returns {Object} A configured api instance
+ * 
+ * @example
+ * 
+ * const myApi = configureApi({ root: 'http://example.com', mode: 'cors' })
+ * 
+ * myApi.get('/thing') // A cors request will be made to "http://example.com/thing"
+ * 
+ * 
+ */
+
 function configureApi (defaults) {
   return api(defaults)
 }
--- src/configure-http.js
+++ src/configure-http.js
@@ -1,5 +1,27 @@
 import http from './http'
 
+/**
+ * 
+ * A function that returns a configured instance of the {@link http} module.
+ * This function's argument will be used as the default config for the returned instance.
+ *
+ * Note: This configuration can always be overridden by passing in options manually.
+ *
+ * @name configureHttp
+ * @type Function
+ * @param {Object} config - An http configuration object
+ * @returns {Object} A configured http instance
+ * 
+ * @example
+ * 
+ * const myHttp = configureHttp({ root: 'http://example.com', mode: 'cors' })
+ * 
+ * myHttp('/thing', { method: 'GET' }) // A cors request will be made to "http://example.com/thing"
+ * 
+ * 
+ */
+
+
 function configureHttp (defaults) {
   return function configuredHttp (endpoint, options) {
     return http(endpoint, { ...defaults, options })
