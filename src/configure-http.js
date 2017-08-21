397d3ab6ca68291368f7a35e3ffbc7cf521cb3a7
David Pickart <davidstoreypickart@gmail.com>
2017-08-21 09:53:25 -0500

    Fix configureHttp

--- src/configure-http.js
+++ src/configure-http.js
@@ -24,7 +24,7 @@ import http from './http'
 
 function configureHttp (defaults) {
   return function configuredHttp (endpoint, options) {
-    return http(endpoint, { ...defaults, options })
+    return http(endpoint, { ...defaults, ...options })
   }
 }
 
