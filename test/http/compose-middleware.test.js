8969450ff8ab435080c9fbab5ac761c3adf734ce
David Pickart <davidstoreypickart@gmail.com>
2019-08-09 13:33:23 -0500

    Override values in middleware rather than deeply merging (#76)
    * Override values from middleware rather than deeply merging
    
    * Version bump: 4.1.2

--- test/http/compose-middleware.test.js
+++ test/http/compose-middleware.test.js
@@ -1,4 +1,5 @@
 import { composeMiddleware } from '../../src'
+import { decamelizeKeys } from 'humps'
 
 describe('composeMiddleware', () => {
   it('applies middleware in correct order', () => {
@@ -15,6 +16,14 @@ describe('composeMiddleware', () => {
     const lowerCase = ({ name }) => ({ name: name.toLowerCase() })
     return composeMiddleware(addName, lowerCase)({}).then(result => {
       expect(result.name).toEqual('david')
+    })
+  })
+  it('overrides prior values rather than merging', () => {
+    expect.assertions(1)
+    const body = { myAttribute: 'foo' }
+    const decamelizeBody = ({ body }) => ({ body: decamelizeKeys(body) })
+    return composeMiddleware(decamelizeBody)({ body }).then(result => {
+      expect(result.body).toEqual({ my_attribute: 'foo' })
     })
   })
 })
