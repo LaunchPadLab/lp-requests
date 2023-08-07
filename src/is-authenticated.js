dae45965e2430eb071002a30a66d9b4ccd5e526e
Conor Hawes <conor@launchpadlab.com>
2023-08-07 13:38:42 -0400

    Remove invisible character

--- src/is-authenticated.js
+++ src/is-authenticated.js
@@ -8,32 +8,32 @@ import { getLpAuthCookie, parseObject } from './utils'
  * token.
  * If the `context` key is present, this function returns true if the user is
  * both authenticated and the specified context is present.
- * 
+ *
  * Note, this does not **validate** the token, it only checks for
  * presence, validation must be done on the server.
- * 
+ *
  * @param {Object} [options={}] - config object containing the context
  * @returns {Boolean}
  * @example
- * 
+ *
  * // WITHOUT context
- * 
+ *
  * // After sign in
  * isAuthenticated() // true
- * 
+ *
  * // After sign out
  * isAuthenticated() // false
- * 
+ *
  * // WITH context
- * 
+ *
  * // After an 'admin' signs in
  * isAuthenticated({ context: 'admin' }) // true
- * 
+ *
  * isAuthenticated({ context: 'non-admin' }) // false
- * 
+ *
  * // After sign out
  * isAuthenticated({ context: 'admin' }) // false
- * 
+ *
  * isAuthenticated({ context: 'non-admin' }) // false
  */
 export default function isAuthenticated (options = {}) {
